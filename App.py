fe3b8ea7132d7c7ae6b64e102a3d55bfe3bf3412
mteo <cortinamateo211@gmail.com>
2026-07-16 12:11:11 -0500

    feat: yes o no
    - siguiendo el tutorial del textual

--- App.py
+++ App.py
@@ -1,15 +1,18 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Welcome
+from textual.widgets import Label, Button
 
 
-class WelcomeApp(App):
+class QuestionApp(App[str]):
     def compose(self) -> ComposeResult:
-        yield Welcome()
+        yield Label("Do you love Textual?")
+        yield Button("Yes", id="yes", variant="primary")
+        yield Button("No", id="no", variant="error")
 
-    def on_button_pressed(self) -> None:
-        self.exit()
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.exit(event.button.id)
 
 
 if __name__ == "__main__":
-    app = WelcomeApp()
-    app.run()
+    app = QuestionApp()
+    reply = app.run()
+    print(reply)
