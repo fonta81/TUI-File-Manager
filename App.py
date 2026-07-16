288c43f42c54ab9437b0030687f7777bbc19745f
mteo <cortinamateo211@gmail.com>
2026-07-16 15:06:45 -0500

    Docs: comentarios
    - se le agregan comentarios para hacer mas sencillo el entender el
    codigo

--- App.py
+++ App.py
@@ -1,3 +1,4 @@
+# se importan las librerias a usar:
 import os
 from pathlib import Path
 from textual.app import App, ComposeResult
@@ -7,8 +8,8 @@ from textual.widgets import Footer
 from textual.widgets import Header
 
 
-class HolaMundo(App):
-    BINDINGS = [
+class HolaMundo(App):  # iniciamos la app
+    BINDINGS = [  # las keys que se usaran, con su definicion:
         Binding(key="q", action="quit", description="Quit the app"),
         Binding(
             key="question_mark",
@@ -21,47 +22,48 @@ class HolaMundo(App):
         Binding(key="k", action="upp", description="Scroll up", show=False),
     ]
 
-    def compose(self) -> ComposeResult:
+    def compose(self) -> ComposeResult:  # lo que se "Imprimira" en la terminal:
         yield DirectoryTree("./")
         yield Footer()
         yield Header()
 
+        # definimos las acciones que se habian asignado en el bindings:
         ###### definicion de acciones ######
 
     def action_help(self) -> None:  # ? -> menu de help
         self.notify("Le da ayuda... se va... epicamente")
 
     def action_delete(self) -> None:  # Del -> elimina
-        tree = self.query_one(DirectoryTree)
+        tree = self.query_one(DirectoryTree)  # guarda la ruta actual
 
-        node = tree.cursor_node
+        node = tree.cursor_node  # guarda la ruta del cursor
 
-        if node is None or node.data is None:
+        if node is None or node.data is None:  # no hay nada -> se sale de la funcion
             self.notify("No hay ningún archivo seleccionado.", severity="warning")
             return
 
-        target_path: Path = node.data.path
+        target_path: Path = node.data.path  # guarda el path del archivo a eliminar
 
         try:
-            if target_path.is_file():
+            if target_path.is_file():  # si es archivo
                 os.remove(target_path)  # Borra el archivo
                 self.notify(f"Archivo eliminado: {target_path.name}")
-            elif target_path.is_dir():
-                os.rmdir(target_path)
+            elif target_path.is_dir():  # si es Carpeta
+                os.rmdir(target_path)  # Borra la Carpeta
                 self.notify(f"Carpeta eliminada: {target_path.name}")
 
-            tree.reload()
+            tree.reload()  # refresca el menu
 
-        except Exception as e:
+        except Exception as e:  # en caso de error:
             self.notify(f"Error al eliminar: {e}", severity="error")
 
     def action_down(self) -> None:  # j -> baja
-        tree = self.query_one(DirectoryTree)
-        tree.action_cursor_down()
+        tree = self.query_one(DirectoryTree)  # cual es el path del cursor
+        tree.action_cursor_down()  # baja el cursor una vez
 
     def action_upp(self) -> None:  # k -> sube
-        tree = self.query_one(DirectoryTree)
-        tree.action_cursor_up()
+        tree = self.query_one(DirectoryTree)  # cual es el path del cursor
+        tree.action_cursor_up()  # sube el cursor una vez
 
 
 if __name__ == "__main__":
