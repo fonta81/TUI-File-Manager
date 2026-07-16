dc242549ea9d37fb62e7e098fe47ae2cc78cb849
mteo <cortinamateo211@gmail.com>
2026-07-16 13:49:34 -0500

    feat: footer
    - estoy probando un menu en la parte de abajao de la "aplicacion"

--- App.py
+++ App.py
@@ -1,10 +1,25 @@
 from textual.app import App, ComposeResult
 from textual.widgets import DirectoryTree
+from textual.binding import Binding
+from textual.widgets import Footer
 
 
 class DirectoryTreeApp(App):
+    BINDINGS = [
+        Binding(key="q", action="quit", description="Quit the app"),
+        Binding(
+            key="question_mark",
+            action="help",
+            description="Show help screen",
+            key_display="?",
+        ),
+        Binding(key="d", action="delete", description="Delete the thing"),
+        Binding(key="j", action="down", description="Scroll down", show=False),
+    ]
+
     def compose(self) -> ComposeResult:
         yield DirectoryTree("./")
+        yield Footer()
 
 
 if __name__ == "__main__":
