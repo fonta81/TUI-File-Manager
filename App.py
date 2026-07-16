b84f2c8ce788853ad9dd4a589208f8a1faa064cc
mteo <cortinamateo211@gmail.com>
2026-07-16 11:28:07 -0500

    feat: welcome
    - siguiendo el tutorial del textual

--- App.py
+++ App.py
@@ -1,29 +1,15 @@
-from textual.app import App
-from textual import events
+from textual.app import App, ComposeResult
+from textual.widgets import Welcome
 
 
-class EventApp(App):
-    COLORS = [
-        "white",
-        "maroon",
-        "red",
-        "purple",
-        "fuchsia",
-        "olive",
-        "yellow",
-        "navy",
-        "teal",
-        "aqua",
-    ]
+class WelcomeApp(App):
+    def compose(self) -> ComposeResult:
+        yield Welcome()
 
-    def on_mount(self) -> None:
-        self.screen.styles.background = "darkblue"
-
-    def on_key(self, event: events.Key) -> None:
-        if event.key.isdecimal():
-            self.screen.styles.background = self.COLORS[int(event.key)]
+    def on_button_pressed(self) -> None:
+        self.exit()
 
 
 if __name__ == "__main__":
-    app = EventApp()
+    app = WelcomeApp()
     app.run()
