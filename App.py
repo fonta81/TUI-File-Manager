77272884bdbcce3e15b64ef3308691b2f43e4231
mteo <cortinamateo211@gmail.com>
2026-07-16 14:22:51 -0500

    feat: up/dawn
    - se agrego la opcion de subir y bajar con -> j, k
    - las funciones del, help, ect imprimen una notificacion en pantalla

--- App.py
+++ App.py
@@ -14,19 +14,31 @@ class HolaMundo(App):
             description="Show help screen",
             key_display="?",
         ),
-        Binding(key="d", action="delete", description="Delete the thing"),
+        Binding(key="delete", action="delete", description="Delete the thing"),
         Binding(key="j", action="down", description="Scroll down", show=False),
+        Binding(key="k", action="upp", description="Scroll up", show=False),
     ]
 
     def compose(self) -> ComposeResult:
         yield DirectoryTree("./")
         yield Footer()
-        yield Header(name="hola mundo")
+        yield Header()
 
         ### definicion de acciones ###
 
-    def action_delete(self) -> None:
-        self.notify("Hola Mundo ")
+    def action_help(self) -> None:  # ? -> menu de help
+        self.notify("Le da ayuda... se va... epicamente")
+
+    def action_delete(self) -> None:  # Del -> elimina
+        self.notify("Se borra un arhivo... epicamente")
+
+    def action_down(self) -> None:  # j -> baja
+        tree = self.query_one(DirectoryTree)
+        tree.action_cursor_down()
+
+    def action_upp(self) -> None:  # k -> sube
+        tree = self.query_one(DirectoryTree)
+        tree.action_cursor_up()
 
 
 if __name__ == "__main__":
